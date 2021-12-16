7118510f8be4dcccb942794beb557105bab9953b
kaigedong <dongkaige@gmail.com>
2021-12-16 17:37:30 +0800

    add doc

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    lang: 'zh-CN',
+    title: 'DBC-Wiki',
+    description: '这是DBC-Wiki站点',
+
+    themeConfig: {
+        logo: 'https://vuejs.org/images/logo.png',
+    },
+    base: '/DBC-Wiki/'
+}
