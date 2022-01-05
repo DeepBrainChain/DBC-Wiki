f8b0fbffad16382e76c2f4444b9c31083d9e5f1b
kaigedong <dongkaige@gmail.com>
2022-01-05 13:55:18 +0800

    update doc

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -128,8 +128,8 @@ module.exports = {
         text: "安装DBC程序",
         link: "/install_and_update_dbc",
         children: [
-          "/install_and_update_dbc/install_update_dbc_client",
           "/install_and_update_dbc/install_dbc_compute_node",
+          "/install_and_update_dbc/install_update_dbc_client",
           "/install_and_update_dbc/update_dbc_compute_node",
         ],
       },
@@ -271,8 +271,8 @@ module.exports = {
         {
           text: "install-dbc",
           children: [
-            "/install_and_update_dbc/install_update_dbc_client",
             "/install_and_update_dbc/install_dbc_compute_node",
+            "/install_and_update_dbc/install_update_dbc_client",
             "/install_and_update_dbc/update_dbc_compute_node",
           ],
         },
