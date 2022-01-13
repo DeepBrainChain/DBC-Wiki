95502856e6daf482c5671dcbd5d6a84e3730a147
kaigedong <dongkaige@gmail.com>
2022-01-13 18:38:22 +0800

    add dbc progress

--- docs/.vuepress/configs/navbar/zh.ts
+++ docs/.vuepress/configs/navbar/zh.ts
@@ -38,6 +38,8 @@ export const zh: NavbarConfig = [
       {
         text: "DBC 进展",
         children: [
+          "/dbc-information/dbc-progress/2020",
+          "/dbc-information/dbc-progress/2021-01-01",
           "/dbc-information/dbc-progress/2021",
           "/dbc-information/dbc-progress/2022-01-01",
         ],
