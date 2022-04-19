832042a0f8474a247df43863011e00a7f753a0c1
q2777 <94602991+q2777@users.noreply.github.com>
2022-04-19 11:33:57 +0800

    Update en.ts

--- docs/.vuepress/configs/sidebar/en.ts
+++ docs/.vuepress/configs/sidebar/en.ts
@@ -156,7 +156,7 @@ export const en = defineSidebarConfig({
         {
           text: "Make image steps (with gpu driver)",
           prefix: "Make image/",
-          children: ["Make-image-steps-(with-gpu-driver)"],
+          children: ["Make-image-steps-(with-gpu-driver).md"],
         },
       ],
     },
