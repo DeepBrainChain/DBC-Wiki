fc175fe3789827180c43f28ae1c50cbb860e08db
kaigedong <dongkaige@gmail.com>
2023-01-29 12:00:02 +0800

    fix: 修复静态资源路径错误

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -2,7 +2,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/",
+  base: "/DBC-Wiki/",
   locales: {
     "/": {
       lang: "zh-CN",
