8bdf6404c330d17e3bf5b90b85cd9e2c9309abe2
ytvchsy <57787806+ytvchsy@users.noreply.github.com>
2024-12-09 18:52:05 +0000

    Deploying to gh-pages from @ DeepBrainChain/DBC-Wiki@7a83b0719647d18df66d464806937f7e43b1c794 🚀

---
+++ assets/mermaid-mindmap.esm.min-6be5ca99.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./app-f2c6a417.js";import"./framework-2c7d842c.js";const t="mindmap",i=a=>a.match(/^\s*mindmap/)!==null,o=async()=>{const{diagram:a}=await r(()=>import("./diagram-definition.0faef4c2-268cad43.js"),[]);return{id:t,diagram:a}},e={id:t,detector:i,loader:o};export{e as default};
